5f43101c5838a589d5a81aac8df8ec26b66d3661
TrevorK-dev <60381884+TrevorK-dev@users.noreply.github.com>
2020-02-13 13:18:18 -0800

    Add files via upload

--- Basic JavaScript Projects/JS/Basic_Javascript_2.js
+++ Basic JavaScript Projects/JS/Basic_Javascript_2.js
@@ -3,3 +3,9 @@ function Function_2() {
     var result = str.fontcolor("red");
     document.getElementById("Red_Text").innerHTML = result;
 }
+
+function myFunction() {
+    var sentance = "I am learning";
+    sentance += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentance;
+}
