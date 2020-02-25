550148c1604b234ec980fffeefcee78156f1538e
TrevorK-dev <60381884+TrevorK-dev@users.noreply.github.com>
2020-02-24 18:07:44 -0800

    Update Basic_JavaScript_5.js

--- Basic JavaScript Projects/JS/Basic_JavaScript_5.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -1,4 +1,14 @@
-function my_Function() {
-    document.getElementById("Test").innerHTML = isNaN('007');
-    document.write(2E310);
+function Ride_Function() {
+    var height, Can_Ride;
+    Height = document.getElementById("Height").value;
+    Can_Ride = (Heigth < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_Ride + " to ride. ";
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = ! (20 > 10);
+}
+
+function yes_Function() {
+    document.getElementById("Yes").innerHTML = ! (5 > 10);
 }
