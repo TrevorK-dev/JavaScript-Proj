4141cbfcd3b54271da7d8d18cf26394330a7e49d
TrevorK-dev <trevor.knisley01@gmail.com>
2020-04-26 16:50:58 -0700

    fixed

--- Basic JavaScript Projects/JS/Basic_Javascript_3.js
+++ Basic JavaScript Projects/JS/Basic_Javascript_3.js
@@ -1,4 +1,12 @@
 function Function_3() {
-    
-    window.alert(math.random() * 100);
+    window.alert(Math.random() * 100);
 }
+
+var x = 6;
+var y = 3;
+
+document.getElementById("demo").innerHTML = 
+(x < 10 && y > 1) + "<br>" + 
+(x < 10 && y < 1);
+
+print(x + y);
