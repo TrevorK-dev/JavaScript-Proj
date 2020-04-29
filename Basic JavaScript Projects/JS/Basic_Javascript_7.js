33cc9660ddbf99cce2d8f3614c40696819ed0ea2
TrevorK-dev <trevor.knisley01@gmail.com>
2020-04-28 21:56:10 -0700

    Added a correct time

--- Basic JavaScript Projects/JS/Basic_Javascript_7.js
+++ Basic JavaScript Projects/JS/Basic_Javascript_7.js
@@ -8,3 +8,18 @@ function Age_Function() {
     }
     document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
+
+function Time_Function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is in the afternoon!";
+    }
+    else {
+        Reply = "It is evening time!";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
